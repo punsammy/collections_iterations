5e2059491c595d535708dfd527b91b9f34db64c8
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-30 14:55:05 -0400

    exercise6.2

---
+++ exercise6.rb
@@ -0,0 +1,5 @@
+#Exercise6.1 Making arrays within a hash
+movies = {:"1999" => ["The Matrix", "Star Wars: Episode 1", "The Mummy"], :"2009" => ["Avatar", "Star Trek", "District 9"], :"2019" => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wards:Episode 9"]}
+
+#Exercise6.2 making array with each row of buttons on phone
+phone_buttons = {:firstrow => [1,2,3], :secondrow => [4,5,6], :thirdrow => [7,8,9], :fourthrow => ["*", 0, "#"]}
