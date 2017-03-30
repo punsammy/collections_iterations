e53a559b1d74daf36888229bda90bcea02f157a6
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-30 12:41:43 -0400

    exercise4.1

---
+++ exercise4.rb
@@ -0,0 +1,21 @@
+#ARRAYS
+fav_colours = ['red', 'purple', 'blue', 'green']
+age_siblings = [22, 24, 26]
+coin_flip = ['heads', 'heads', 'tails', 'heads', 'tails']
+fav_artists = ['drake', 'britney', 'beyonce']
+fav_colours = [:red, :purple, :blue, :green]
+
+#HASHES
+words_defintions = {:big => "of considerable size", :medium => "not big, not small", :small => "size less than normal" }
+
+fav_movies = {:rush_hour1 => 1998, :rush_hour2 => 2001, :rush_hour3 => 2007}
+
+cities = {:brampton => 588000, :missisauga => 758000, :toronto => 2810000}
+
+siblings = { :ashton => 26, :nick => 22 }
+coin_flip = ['heads', 'heads', 'tails', 'heads', 'tails']
+
+#Exercise4.1 Print ages of siblings less than 24
+age_siblings.each do |age|
+  puts age if (age < 24)
+end
