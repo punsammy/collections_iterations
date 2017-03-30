05935b004022ed3f9db8842901b45fb09c460b84
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-30 15:30:01 -0400

    exercise6b.2

--- exercise6b.rb
+++ exercise6b.rb
@@ -5,6 +5,24 @@
 end
 
 # #Method 2 Another way to do it
-# puts ("I will not skateboard in the halls" * 20)
+puts ("I will not skateboard in the halls" * 20)
 
-#
+#Exercise6b.2
+#Method 1
+lines = []
+20.times do
+  lines << "I will not skateboard in the halls"
+end
+
+puts lines
+
+# #Method 2
+array = []
+(1..20).each do
+  array << "I will not skateboard in the halls"
+end
+print array
+
+#Method 3
+ary = Array.new
+Array.new(20, "I will not skateboard in the halls")
