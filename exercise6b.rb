a74dfe7830e045f38c663ac8327424fd04dc6e49
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-30 15:40:38 -0400

    exercise6b.3

--- exercise6b.rb
+++ exercise6b.rb
@@ -5,24 +5,25 @@
 end
 
 # #Method 2 Another way to do it
-puts ("I will not skateboard in the halls" * 20)
+# puts ("I will not skateboard in the halls" * 20)
 
-#Exercise6b.2
+#Exercise6b.2 make array that prints line 20x
 #Method 1
-lines = []
-20.times do
-  lines << "I will not skateboard in the halls"
-end
-
-puts lines
-
-# #Method 2
 array = []
-(1..20).each do
+20.times do
   array << "I will not skateboard in the halls"
 end
-print array
 
-#Method 3
-ary = Array.new
-Array.new(20, "I will not skateboard in the halls")
+puts array
+
+# #Method 2
+# ary = Array.new
+# Array.new(20, "I will not skateboard in the halls")
+
+#Exercise6b.3 array with numbers 1-50
+numbers = []
+(1..50).each do |num|
+  numbers << num
+end
+
+puts numbers
