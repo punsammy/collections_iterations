fa7d268635f9eff903304196a2d98b93c8ef838c
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-30 15:19:20 -0400

    exercise6b.1

---
+++ exercise6b.rb
@@ -0,0 +1,10 @@
+#Exercise6b.1
+#Method 1 each prints on different line
+20.times do
+  puts "I will not skateboard in the halls"
+end
+
+# #Method 2 Another way to do it
+# puts ("I will not skateboard in the halls" * 20)
+
+#
