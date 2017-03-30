7f91cc69c8e87f56aef362f83d9fbd9b8e1ad8c4
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-30 11:44:10 -0400

    exercise3.2

---
+++ exercise3.rb
@@ -0,0 +1,24 @@
+#ARRAYS
+fav_colours = ['red', 'purple', 'blue', 'green']
+age_sibilings = [22, 26]
+coin_flip = ['heads', 'heads', 'tails', 'heads', 'tails']
+fav_artists = ['drake', 'britney', 'beyonce']
+fav_colours = [:red, :purple, :blue, :green]
+
+#HASHES
+words_defintions = {:big => "of considerable size", :medium => "not big, not small", :small => "size less than normal" }
+
+fav_movies = {:"Rush Hour" => 1998, :"Rush Hour 2" => 2001, :"Rush Hour 3" => 2007}
+
+cities = {:brampton => 588000, :missisauga => 758000, :toronto => 2810000}
+
+siblings = { :ashton => 26, :nick => 22 }
+coin_flip = ['heads', 'heads', 'tails', 'heads', 'tails']
+
+#Exercise3.1 printing out first 2 fav_artists
+puts fav_artists[0..1]
+
+#Exercise3.2 printing fav_movies and year it was released
+fav_movies.each do |key, value|
+  puts "#{key} was initially released in #{value}"
+end
